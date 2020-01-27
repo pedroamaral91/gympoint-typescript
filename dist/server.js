290aab45314c5d7478d1a9d05b6db03f49dc12aa
pedroamaral91 <pedrovinicius91@gmail.com>
2020-01-27 09:33:31 -0300

    added github CI

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-
-const server = new (0, _app2.default)(_express2.default.call(void 0, ))
-
-server.server.listen(3333)
